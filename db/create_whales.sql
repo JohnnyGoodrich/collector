aef45d1f4c98a1e3ba9fd5b57705bd4342ae7603
JohnnyGoodrich <jbgoodrich26@gmail.com>
2023-01-27 09:01:06 -0500

    finished lab

---
+++ db/create_whales.sql
@@ -0,0 +1,23 @@
+INSERT INTO main_app_whale(name, img, bio, verified_whale, created_at)
+  VALUES
+    ('Orca Whale', 'https://a-z-animals.com/media/killer-whale-1.jpg', 'The orca or killer whale (Orcinus orca) is a toothed whale belonging to the oceanic dolphin family, of which it is the largest member.','t','2021-05-07 18:57:42.517205-07');
+
+INSERT INTO main_app_whale(name, img, bio, verified_whale, created_at)
+  VALUES
+    ('Humpback Whale','https://media.fisheries.noaa.gov/styles/original/s3/2021-08/640x427-Whale-Humpback-watermark.jpg?itok=3RBJ1uQq','The humpback whale (Megaptera novaeangliae) is a species of baleen whale. It is a rorqual (a member of the family Balaenopteridae) and is the only species in the genus Megaptera.','f','2021-05-07 18:57:42.517205-07');
+
+INSERT INTO main_app_whale(name, img, bio, verified_whale, created_at)
+  VALUES
+    ('Blue Whale','http://www.nwf.org/-/media/NEW-WEBSITE/Shared-Folder/Wildlife/Mammals/mammal_blue-whale_eco2drew-GettyImages_600x300.ashx','The blue whale (Balaenoptera musculus) is a marine mammal and a baleen whale. Reaching a maximum confirmed length of 29.9 meters (98 ft) and weighing up to 199 tonnes (196 long tons; 219 short tons), it is the largest animal known to have ever existed. ','f','2021-05-07 18:57:42.517205-07');
+
+INSERT INTO main_app_whale(name, img, bio, verified_whale, created_at)
+  VALUES
+    ('Beluga Whale','https://s.hdnux.com/photos/01/26/26/25/22635855/4/1200x0.jpg','The beluga whale (Delphinapterus leucas) is an Arctic and sub-Arctic cetacean. It is one of two members of the family Monodontidae, along with the narwhal, and the only member of the genus Delphinapterus.','t','2021-05-07 18:57:42.517205-07');
+
+INSERT INTO main_app_whale(name, img, bio, verified_whale, created_at)
+  VALUES
+    ('Narwhal','http://ocean.si.edu/sites/default/files/styles/article_main_image/public/%28GW%29-narwhal-tusk.jpg.webp?itok=GQrGvXsE','The narwhal, also known as a narwhale (Monodon monoceros), is a medium-sized toothed whale that possesses a large "tusk" from a protruding canine tooth. It lives year-round in the Arctic waters around Greenland, Canada and Russia. It is one of two living species of whale in the family Monodontidae, along with the beluga whale, and the only species in the genus Monodon.','t','2021-05-07 18:57:42.517205-07');
+
+INSERT INTO main_app_whale(name, img, bio, verified_whale, created_at)
+  VALUES
+    ('Sperm Whale','https://static01.nyt.com/images/2022/10/02/books/review/02Backpage_upclose-06/02Backpage_upclose-06-articleLarge.jpg?quality=75&auto=webp&disable=upscale','The sperm whale or cachalot[a] (Physeter macrocephalus) is the largest of the toothed whales and the largest toothed predator. It is the only living member of the genus Physeter and one of three extant species in the sperm whale family, along with the pygmy sperm whale and dwarf sperm whale of the genus Kogia.','t','2021-05-07 18:57:42.517205-07');
--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.Home.as_view(), name="home"),
     path('about/', views.About.as_view(), name="about"), # <- new route
-    path('whales/', views.WhaleList.as_view(), name="whales_list")
+    path('whales/', views.WhaleList.as_view(), name="whales_list"),
+    path('whales/new/', views.WhaleCreate.as_view(), name="whales_create"),
+    path('whales/<int:pk>/', views.WhaleDetail.as_view(), name="whale_detail"),
+    path('whales/<int:pk>/update',views.WhaleUpdate.as_view(), name="whale_update"),
+    path('whales/<int:pk>/delete',views.WhaleDelete.as_view(), name="whale_delete"),
 ]
