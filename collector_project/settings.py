714fba08d702082c259be8376ddb1b246fbc5e37
JohnnyGoodrich <jbgoodrich26@gmail.com>
2023-05-04 11:46:14 -0400

    attempting to deploy with gunicorn

--- collector_project/settings.py
+++ collector_project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-216-kx%cw=42u&=*jo17w4buop)h_-+0%l%6fb3z3v8j9#rkb='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','sampledomain.com']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
