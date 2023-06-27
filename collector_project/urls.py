9f55968e38cedb0f35a03d96e93d62e13fc75474
JohnnyGoodrich <jbgoodrich26@gmail.com>
2023-06-27 14:36:23 -0400

    test

--- collector_project/urls.py
+++ collector_project/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include # <- you must add include to the imports
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main_app.urls')), # <- here is the new line to include the urls of our app
+    path('whales/', include('main_app.urls'))
 
     # url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
     # url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
